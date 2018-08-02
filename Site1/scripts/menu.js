7e4aaa5f18b339c61cd5ea2bda6be05659fff9a5
Kalyan Karumuri <Kalyan.Karumuri@footlocker.com>
2018-08-02 05:57:20 +0530

    Initial Commit

---
+++ Site1/scripts/menu.js
@@ -0,0 +1,38 @@
+
+function mmLoadMenus() {
+  if (window.mm_menu_1120124958_0) return;
+
+window.mm_menu_1120124958_0 = new Menu("root",87,NaN,"Arial, Helvetica, sans-serif",11,"#000000","#FFFFFF","#F3F3F3","#671a00","left","middle",6,0,100,-5,7,true,true,true,0,false,false);
+  /*  mm_menu_1120124958_0.addMenuItem("Our&nbsp;Mission","location='about-us.html'");
+mm_menu_1120124958_0.addMenuItem("Employees","location='employees.html'");
+  mm_menu_1120124958_0.addMenuItem("Testimonials","location='testimonials.html'");*/
+   mm_menu_1120124958_0.hideOnMouseOut=true;
+   mm_menu_1120124958_0.bgColor='#DFDFDF';
+   mm_menu_1120124958_0.menuBorder=1;
+   mm_menu_1120124958_0.menuLiteBgColor='#FFFFFF';
+   mm_menu_1120124958_0.menuBorderBgColor='#DBDBDB';
+
+
+window.mm_menu_1120135636_0 = new Menu("root",85,23,"Arial, Helvetica, sans-serif",11,"#000000","#FFFFFF","#F3F3F3","#671a00","left",	  "middle",3,0,100,-5,7,true,true,true,0,true,false);
+  mm_menu_1120135636_0.addMenuItem("Development","location='development.html'");
+  mm_menu_1120135636_0.addMenuItem("Maintenance","location='maintenance.html'");
+  mm_menu_1120135636_0.addMenuItem("Consulting","location='consulting.html'");
+  /*mm_menu_1120135636_0.addMenuItem("&nbsp;Call&nbsp;Center","location='callcenter.html'");*/
+   mm_menu_1120135636_0.hideOnMouseOut=true;
+   mm_menu_1120135636_0.bgColor='#DFDFDF';
+   mm_menu_1120135636_0.menuBorder=1;
+   mm_menu_1120135636_0.menuLiteBgColor='#FFFFFF';
+   mm_menu_1120135636_0.menuBorderBgColor='#DBDBDB';
+   
+  window.mm_menu_1120140443_0 = new Menu("root",180,23,"Arial, Helvetica, sans-serif",11,"#000000","#FFFFFF","#F3F3F3","#671a00","left","middle",3,0,100,-5,7,true,true,true,0,false,false);
+ /* mm_menu_1120140443_0.addMenuItem("&nbsp;QA&nbsp;&&nbsp;Automation&nbsp;Practice&nbsp;","location='qa.html'");
+  mm_menu_1120140443_0.addMenuItem("&nbsp;DW&nbsp;Practice","location='dwpractice.html'");
+  mm_menu_1120140443_0.addMenuItem("&nbsp;SAP&nbsp;Practice","location='sappractice.html'");*/
+   mm_menu_1120140443_0.hideOnMouseOut=true;
+   mm_menu_1120140443_0.bgColor='#DFDFDF';
+   mm_menu_1120140443_0.menuBorder=1;
+   mm_menu_1120140443_0.menuLiteBgColor='#FFFFFF';
+   mm_menu_1120140443_0.menuBorderBgColor='#DBDBDB';
+
+mm_menu_1120140443_0.writeMenus();
+} 
